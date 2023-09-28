43646f3bf5512d802b54c957e5deb68203a7eea8
Junaid Ul Haque Sheikh <145222963+junaidsheikh02@users.noreply.github.com>
2023-09-27 21:18:08 -0700

    Exercise 0001
    // Store the person's name in a variable
    const personName: string = "Eric";
    
    // Print a personalized message
    console.log(`Hello ${personName}, would you like to learn some TypeScript today?`);

---
+++ personName.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Store the person's name in a variable
+const personName = "Eric";
+// Print a personalized message
+console.log(`Hello ${personName}, would you like to learn some TypeScript today?`);
---
+++ personName.ts
@@ -0,0 +1,5 @@
+// Store the person's name in a variable
+const personName: string = "Eric";
+
+// Print a personalized message
+console.log(`Hello ${personName}, would you like to learn some TypeScript today?`);
