ebbe95e76713f0986d152c2cce4a5fb39a5dbd40
Junaid Ul Haque Sheikh <145222963+junaidsheikh02@users.noreply.github.com>
2023-09-28 03:01:25 -0700

    Exercise 0002
    CONVERT INTO TS FILES IN VISUAL STUDIO CODE:
     // Store the person's name in a variable with type annotation
    const personName: string = "Eric";
    // Create a personalized message
    const message: string = `Hello ${personName}, would you like to learn some Python today?`;
    
    // Print the message
    console.log(message);
    
    
    
    CONVERT INTO JS FILES IN VISUAL STUDIO CODE:
    "use strict";
    Object.defineProperty(exports, "__esModule", { value: true });
    // Store the person's name in a variable with type annotation
    const personName = "Eric";
    // Create a personalized message
    const message = `Hello ${personName}, would you like to learn some Python today?`;
    // Print the message
    console.log(message);

--- personName.js
+++ personName.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// Store the person's name in a variable
+// Store the person's name in a variable with type annotation
 const personName = "Eric";
-// Print a personalized message
-console.log(`Hello ${personName}, would you like to learn some TypeScript today?`);
+// Create a personalized message
+const message = `Hello ${personName}, would you like to learn some Python today?`;
+// Print the message
+console.log(message);
--- personName.ts
+++ personName.ts
@@ -1,5 +1,10 @@
-// Store the person's name in a variable
+// Store the person's name in a variable with type annotation
 const personName: string = "Eric";
 
-// Print a personalized message
-console.log(`Hello ${personName}, would you like to learn some TypeScript today?`);
+// Create a personalized message
+const message: string = `Hello ${personName}, would you like to learn some Python today?`;
+
+// Print the message
+console.log(message);
+
+
