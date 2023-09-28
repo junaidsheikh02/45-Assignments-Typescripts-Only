fe55e2421398e63e2801be10170fc14ec8d59740
Junaid Ul Haque Sheikh <145222963+junaidsheikh02@users.noreply.github.com>
2023-09-28 04:28:28 -0700

    EXERCISE 0004
    CONVERT PYTHON CODE INTO TYPESCRIPT CODE:
    // Store the quote and its author in variables with type annotations
    const quote: string = "A person who never made a mistake never tried anything new.";
    const author: string = "Albert Einstein";
    // Print the quote with the author's name
    console.log(`${author} once said, "${quote}"`);
    
    
    
    CONVERT PYTHON CODE INTO JAVASCRIPT CODE:
    "use strict";
    Object.defineProperty(exports, "__esModule", { value: true });
    // Store the quote and its author in variables with type annotations
    const quote = "A person who never made a mistake never tried anything new.";
    const author = "Albert Einstein";
    // Print the quote with the author's name
    console.log(`${author} once said, "${quote}"`);

---
+++ famousQuote.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Store the quote and its author in variables with type annotations
+const quote = "A person who never made a mistake never tried anything new.";
+const author = "Albert Einstein";
+// Print the quote with the author's name
+console.log(`${author} once said, "${quote}"`);
---
+++ famousQuote.ts
@@ -0,0 +1,7 @@
+// Store the quote and its author in variables with type annotations
+const quote: string = "A person who never made a mistake never tried anything new.";
+const author: string = "Albert Einstein";
+
+// Print the quote with the author's name
+console.log(`${author} once said, "${quote}"`);
+
