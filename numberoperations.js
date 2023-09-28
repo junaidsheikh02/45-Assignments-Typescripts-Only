bb14d39c57094d7268529c834908637ea4e01fc9
Junaid Ul Haque Sheikh <145222963+junaidsheikh02@users.noreply.github.com>
2023-09-28 07:03:50 -0700

    EXERCISE:>0009
    CONVERT PYTHON CODE INTO TYPESCRIPT CODE:>
    // Store your favorite number in a variable
    const favorite_number: number = 42;
    
    // Create a message that reveals your favorite number
    const message: string = `My favorite number is ${favorite_number}.`;
    
    // Print the message
    console.log(message);
    
    
    
    
    
    CONVERT PYTHON CODE INTO JAVASCRIPT CODE:>
    "use strict";
    // Store your favorite number in a variable
    const favorite_number = 42;
    // Create a message that reveals your favorite number
    const message = `My favorite number is ${favorite_number}.`;
    // Print the message
    console.log(message);

--- numberoperations.js
+++ numberoperations.js
@@ -1,5 +1,7 @@
 "use strict";
-console.log(8);
-console.log(8);
-console.log(8);
-console.log(8);
+// Store your favorite number in a variable
+const favorite_number = 42;
+// Create a message that reveals your favorite number
+const message = `My favorite number is ${favorite_number}.`;
+// Print the message
+console.log(message);
--- numberoperations.ts
+++ numberoperations.ts
@@ -1,4 +1,8 @@
-console.log(8);
-console.log(8);
-console.log(8);
-console.log(8);
+// Store your favorite number in a variable
+const favorite_number: number = 42;
+
+// Create a message that reveals your favorite number
+const message: string = `My favorite number is ${favorite_number}.`;
+
+// Print the message
+console.log(message);
