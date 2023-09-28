659d1c643d28ecb69291dbce626c4ad047ac11e4
Junaid Ul Haque Sheikh <145222963+junaidsheikh02@users.noreply.github.com>
2023-09-28 04:39:06 -0700

    EXERCISE 0005
    CONVERT PYTON CODE INTO TYPESCRIPT CODE:>
    // Step 1: Store the famous person's name in a variable
    const famous_person: string = "Albert Einstein";
    
    // Step 2: Compose your message using the famous quote and the famous person's name
    const message: string = famous_person + " once said, 'Imagination is more important than knowledge.'";
    
    // Step 3: Print your message
    console.log(message);
    
    
    CONVERT PYTON CODE INTO JAVASCRIPT CODE:>
    // Step 1: Store the famous person's name in a variable
    const famous_person = "Albert Einstein";
    
    // Step 2: Compose your message using the famous quote and the famous person's name
    const message = famous_person + " once said, 'Imagination is more important than knowledge.'";
    
    // Step 3: Print your message
    console.log(message);

--- famousQuote.js
+++ famousQuote.js
@@ -1,7 +1,8 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Store the quote and its author in variables with type annotations
-const quote = "A person who never made a mistake never tried anything new.";
-const author = "Albert Einstein";
-// Print the quote with the author's name
-console.log(`${author} once said, "${quote}"`);
+// Step 1: Store the famous person's name in a variable
+const famous_person = "Albert Einstein";
+
+// Step 2: Compose your message using the famous quote and the famous person's name
+const message = famous_person + " once said, 'Imagination is more important than knowledge.'";
+
+// Step 3: Print your message
+console.log(message);
--- famousQuote.ts
+++ famousQuote.ts
@@ -1,7 +1,8 @@
-// Store the quote and its author in variables with type annotations
-const quote: string = "A person who never made a mistake never tried anything new.";
-const author: string = "Albert Einstein";
+// Step 1: Store the famous person's name in a variable
+const famous_person: string = "Albert Einstein";
 
-// Print the quote with the author's name
-console.log(`${author} once said, "${quote}"`);
+// Step 2: Compose your message using the famous quote and the famous person's name
+const message: string = famous_person + " once said, 'Imagination is more important than knowledge.'";
 
+// Step 3: Print your message
+console.log(message);
